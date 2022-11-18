bc503970d0d049076f9c1b32fe37deafdff60219
Saulight73 <ninimeme73@gmail.com>
2022-11-18 15:06:45 +0100

    adding login info

--- index.php
+++ index.php
@@ -1,8 +1,14 @@
 <?php
 
 include './Core/Class/core.php';
+$type = $_POST["type"];
+if($type =="login"){
+    $login = $_POST["login"];
+    $password = $_POST["password"];
+    $myclass = new core();
+    $oui = $myclass->loginRequest($_POST['login'],$_POST['password']);
+    echo"<br><br>";
+    echo($oui);
+    
+}
 
-$myclass = new core();
-$oui = $myclass->loginRequest("maurice","bob");
-echo"<br><br>";
-echo($oui);
